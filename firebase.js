2e215da16e587986900bcddbd2f5652640b457fa
poldani2024 <pedro.l.oldani@gmail.com>
2025-12-08 23:47:26 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,33 +1,25 @@
-// Cargar Firebase Core
-var scriptApp = document.createElement("script");
-scriptApp.src = "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-document.head.appendChild(scriptApp);
+// SDK Firebase versión compat (funciona en cualquier navegador)
+document.write(`
+  <script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js"></scr` + `ipt>
+  <script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-auth-compat.js"></scr` + `ipt>
+  <script src="https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore-compat.js"></scr` + `ipt>
+`);
 
-// Cargar Firebase Auth
-var scriptAuth = document.createElement("script");
-scriptAuth.src = "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-document.head.appendChild(scriptAuth);
+window.addEventListener("load", () => {
+  // Tu configuración de Firebase
+  const firebaseConfig = {
+    apiKey: "AlzaSyB6477kA_dp_17XWE2Al8gSAIXRKG_NKSY",
+    authDomain: "reportevecinal.firebaseapp.com",
+    projectId: "reportevecinal",
+    storageBucket: "reportevecinal.firebasestorage.app",
+    messagingSenderId: "42320775776",
+    appId: "1:42320775776:web:8e5305fba08d19bc8e5105"
+  };
 
-// Cargar Firestore
-var scriptFirestore = document.createElement("script");
-scriptFirestore.src = "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-document.head.appendChild(scriptFirestore);
+  firebase.initializeApp(firebaseConfig);
 
-// Inicializar Firebase cuando todo cargue
-scriptFirestore.onload = function () {
-    const firebaseConfig = {
-        apiKey: "AlzaSyB6477kA_dp_17XWE2Al8gSAIXRKG_NKSY",
-        authDomain: "reportevecinal.firebaseapp.com",
-        projectId: "reportevecinal",
-        storageBucket: "reportevecinal.firebasestorage.app",
-        messagingSenderId: "42320775776",
-        appId: "1:42320775776:web:8e5305fba08d19bc8e5105"
-    };
+  window.auth = firebase.auth();
+  window.db = firebase.firestore();
 
-    firebase.initializeApp(firebaseConfig);
-
-    window.auth = firebase.auth();
-    window.db = firebase.firestore();
-
-    console.log("🔥 Firebase cargado correctamente");
-};
+  console.log("🔥 Firebase compat cargado y listo");
+});
