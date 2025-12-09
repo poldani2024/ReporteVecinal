e6cbd6b6218ebb186ca27c71a1e8af248c0fd085
poldani2024 <pedro.l.oldani@gmail.com>
2025-12-09 11:27:08 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 // Inicializar Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyB6477kA_dp_17XWE2Al8gSAIXRKG_NKSY",
-  authDomain: "reportvecinal.firebaseapp.com",
-  projectId: "reportvecinal",
-  storageBucket: "reportvecinal.firebasestorage.app",
+  authDomain: "reportevecinal.firebaseapp.com",
+  projectId: "reportevecinal",
+  storageBucket: "reportevecinal.firebasestorage.app",
   messagingSenderId: "42320775776",
   appId: "1:42320775776:web:70d9206815f650b98e5105"
 };
