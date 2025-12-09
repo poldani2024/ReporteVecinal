da7075cd2d48c0dd1dc3da4d7b5a5455afe5a672
poldani2024 <pedro.l.oldani@gmail.com>
2025-12-09 11:03:40 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// Inicializar Firebase
+const firebaseConfig = {
+  apiKey: "AlzaSyB6477kA_dp_17XWE2Al8gSAIXRKG_NKSY",
+  authDomain: "reportvecinal.firebaseapp.com",
+  projectId: "reportvecinal",
+  storageBucket: "reportvecinal.firebasestorage.app",
+  messagingSenderId: "42320775776",
+  appId: "1:42320775776:web:8e5305fba08d19bc8e5105"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+window.auth = firebase.auth();
+window.db = firebase.firestore();
+
+console.log("🔥 Firebase cargado correctamente");
