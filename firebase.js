5e30de338f0af4657ac5c1d9907aa79653484aa9
poldani2024 <pedro.l.oldani@gmail.com>
2025-12-26 13:10:02 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+
+// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyB6477kA_dp_17XWE2Al8gSAIXRKG_NKSY",
   authDomain: "reportevecinal.firebaseapp.com",
@@ -7,11 +9,17 @@ const firebaseConfig = {
   appId: "1:42320775776:web:70d9206815f650b98e5105"
 };
 
-
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
+// Persistencia offline (si el navegador lo soporta)
+firebase.firestore().enablePersistence({ synchronizeTabs: true })
+  .then(() => console.debug('[firestore] offline persistence OK'))
+  .catch(err => {
+    console.warn('[firestore] persistence error:', err.code || err.message);
+    // En Safari privado o sin IndexedDB: seguimos online sin caché.
+  });
 
 window.auth = firebase.auth();
 window.db = firebase.firestore();
